ec95e89b3db031bf2d632d1df9cdc62a33eae64a
julieluangpraxay <julieluangpraxay@gmail.com>
2024-01-03 13:06:36 -0800

    Added tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,13 @@
 
 import './App.css'
+import Navigation from './Components/Navigation'
 
 function App() {
 
   return (
     <>
-<h1>this is a test</h1>  
-<h2>react app test</h2> 
-<h2>hello!!</h2>
-<p>hello there</p>
-<p>test part 2</p>
- </>
+    <Navigation/>
+    </>
   )
 }
 
---
+++ src/Components/Navigation.tsx
@@ -0,0 +1,14 @@
+export default function Navigation() {
+    return (
+        <>
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>Projects</li>
+                <li>Resume</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+        </>
+    )
+}
