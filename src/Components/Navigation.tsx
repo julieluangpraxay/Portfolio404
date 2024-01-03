4f52402733c8bafe6039de56a0540f3ca9e94eba
julieluangpraxay <julieluangpraxay@gmail.com>
2024-01-03 14:05:13 -0800

    WIP

--- src/Components/Navigation.tsx
+++ src/Components/Navigation.tsx
@@ -1,9 +1,10 @@
+import '/src/index.css'
 
 export default function Navigation() {
     return (
         <>
         <nav>
-            <ul className="list-none text-pink-400">
+            <ul className="list-none">
                 <li>Home</li>
                 <li>Projects</li>
                 <li>Resume</li>
