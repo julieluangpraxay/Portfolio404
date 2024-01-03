aaba90989585379c4d1cdd86e453e8e7ec7d5cbc
julieluangpraxay <julieluangpraxay@gmail.com>
2024-01-03 13:20:29 -0800

    Fixed tailwind issue

--- src/Components/Navigation.tsx
+++ src/Components/Navigation.tsx
@@ -1,8 +1,10 @@
+import './App.css';
+
 export default function Navigation() {
     return (
         <>
         <nav>
-            <ul>
+            <ul className="list-none">
                 <li>Home</li>
                 <li>Projects</li>
                 <li>Resume</li>
